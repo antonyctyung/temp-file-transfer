b7dd660ea302c7ad7b3fcb5643a6d43b8b1714b5
Antony Yung <22206795+antonyctyung@users.noreply.github.com>
2022-04-14 17:48:47 -0700

    use thread in c code

--- project/clang/rc6_test.c
+++ project/clang/rc6_test.c
@@ -1,9 +1,10 @@
 #include <stdint.h>
 #include "rc6.h"
+#include "pthread.h" 
 
-int main()
+
+void *uut()
 {
-    const int ROUND = 1000;
     uint8_t plaintext[16] = {   0x02, 0x13, 0x24, 0x35, 0x46, 0x57, 0x68, 0x79, 
                                 0x8a, 0x9b, 0xac, 0xbd, 0xce, 0xdf, 0xe0, 0xf1  };
     uint8_t cyphertext[16] = {  0xc8, 0x24, 0x18, 0x16, 0xf0, 0xd7, 0xe4, 0x89,
@@ -12,12 +13,25 @@ int main()
                         0x01, 0x12, 0x23, 0x34, 0x45, 0x56, 0x67, 0x78,
                         0x89, 0x9a, 0xab, 0xbc, 0xcd, 0xde, 0xef, 0xf0, 
                         0x10, 0x32, 0x54, 0x76, 0x98, 0xba, 0xdc, 0xfe  };
-    
-    for (int i = 0; i < ROUND; i++)
+    RC6_32_20_32_encrypt(plaintext,32,key); //c824 ... 5d48
+    RC6_32_20_32_decrypt(cyphertext,32,key); //0213 ... e0f1
+}
+
+int main()
+{
+    const int ROUND = 1000;
+    pthread_t pthrd[8];
+
+    for (int i = 0; i < ROUND/8; i++)
     {
-        uint32_t* S = key_expansion(32,key);
-        RC6_32_20_32_encrypt(plaintext,32,key); //c824 ... 5d48
-        RC6_32_20_32_decrypt(cyphertext,32,key); //0213 ... e0f1
+        for (int j = 0; j < 8; j++)
+        {
+            pthread_create(&(pthrd[i]), NULL, uut, NULL);
+        }
+        for (int j = 0; j < 8; j++)
+        {
+            pthread_join(pthrd[i], NULL);
+        }
     }
     return 1;
 }
--- project/genstat.sh
+++ project/genstat.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 cd clang
+mkdir bin
+mkdir obj
 make
 cd ..
 ./stat_script.sh > stat.txt 2>&1
